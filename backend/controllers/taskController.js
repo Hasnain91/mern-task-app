abaa9d9fafd60405176e6e818483ea3503d830d8
Hasnain91 <hasnainrao@yahoo.com>
2024-03-12 15:38:45 +0500

    added "delet a task"

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -20,7 +20,7 @@ const getTasks = async (req, res) => {
   }
 };
 
-// Get a task
+// Get a single task
 const getTask = async (req, res) => {
   try {
     const { id } = req.params;
@@ -34,8 +34,22 @@ const getTask = async (req, res) => {
   }
 };
 
+const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findByIdAndDelete(id);
+    if (!task) {
+      res.status(404).json(`No task with id: ${id}`);
+    }
+    res.status(200).send("Task Deleted Successfully!");
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
 module.exports = {
   createTask,
   getTasks,
   getTask,
+  deleteTask,
 };
--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -5,10 +5,12 @@ const {
   createTask,
   getTasks,
   getTask,
+  deleteTask,
 } = require("../controllers/taskController");
 
 router.post("/api/tasks", createTask);
 router.get("/api/tasks", getTasks);
 router.get("/api/tasks/:id", getTask);
+router.delete("/api/tasks/:id", deleteTask);
 
 module.exports = router;
