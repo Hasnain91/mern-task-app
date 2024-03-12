355c632461090b0a58d3b8a3bbed8bf74c877346
Hasnain91 <hasnainrao@yahoo.com>
2024-03-12 14:43:53 +0500

    created routes folder and separated routes from server

--- backend/server.js
+++ backend/server.js
@@ -3,35 +3,18 @@ const express = require("express");
 const connectDB = require("./config/connectDB");
 const mongoose = require("mongoose");
 const Task = require("./model/taskModel");
+const taskRoutes = require("./routes/taskRoute");
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(taskRoutes);
+
 // Routes
 app.get("/", (req, res) => {
   res.send("Home Page...");
-});
-
-// Create a Task
-app.post("/api/tasks", async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(200).json(task);
-  } catch (error) {
-    res.status(500).json({ msg: error.message });
-  }
-});
-
-// Get/Read tasks
-app.get("/api/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find();
-    res.status(200).json(tasks);
-  } catch (error) {
-    res.status(500).json({ msg: error.message });
-  }
 });
 
 const PORT = process.env.PORT || 3000;
