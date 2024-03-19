2407b54533e5981f3e97f2c9f5f2412adb9bffe1
Hasnain91 <hasnainrao@yahoo.com>
2024-03-19 15:47:17 +0500

    Updated a link

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:3000/", "https://mern-task-app.render.com/"],
+    origin: [
+      "http://localhost:3000/",
+      "https://mern-task-app-1o0a.onrender.com/",
+    ],
   })
 );
 
